80f00459b0bd137ac81a651bbc68ecabc58dfab7
look4green <look4green@users.noreply.github.com>
2025-07-22 16:41:58 +0800

    Create SubGHzTools.h

---
+++ SubGHzTools.h
@@ -0,0 +1,12 @@
+#ifndef SUBGHZTOOLS_H
+#define SUBGHZTOOLS_H
+
+#include <Arduino.h>
+
+void initSubGHz();
+void startReplayAttack();
+void stopReplayAttack();
+void jamSubGHz();
+void captureSubGHzSignal();
+
+#endif
