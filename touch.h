d43d2ddfe36a328addc2ed6bbe0272ffea7052c9
look4green <look4green@users.noreply.github.com>
2025-07-23 00:05:26 +0800

    Update touch.h

--- touch.h
+++ touch.h
@@ -1,1 +1,15 @@
 
+#ifndef TOUCHSCREEN_H
+#define TOUCHSCREEN_H
+
+#include <Arduino.h>
+
+// Touchscreen API
+void initTouchscreen();
+bool isTouched();
+int getTouchX();
+int getTouchY();
+void updateTouch();          // Polls and stores latest point
+bool touchClicked();         // One-time tap detection
+
+#endif
