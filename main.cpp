dc37a7cbdde40a63888d1c0422c95a2b8ace5125
look4green <look4green@users.noreply.github.com>
2025-07-22 16:18:45 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "Encoder.h"
+#include "Touchscreen.h"
+#include "Display.h"
+#include "Menu.h"
+#include "AppState.h"
+#include "Storage.h"
+
+void setup() {
+  Serial.begin(115200);
+
+  initDisplay();
+  initEncoder();
+  initTouchscreen();
+  initStorage();
+  initMenu();
+  initAppState();
+}
+
+void loop() {
+  updateEncoder();
+
+  // Rotary input
+  if (encoderMoved()) {
+    int delta = getEncoderValue();
+    updateMenu(delta);  // Scroll menu
+  }
+
+  if (encoderClicked()) {
+    selectMenuItem();   // Confirm selection
+    updateAppState();   // Execute selected mode
+  }
+
+  // Touch input
+  TS_Point p = getTouchPoint();
+  if (p.x != -1 && p.y != -1) {
+    int zone = getTouchZone(p.x, p.y);
+    if (zone != -1) {
+      updateMenu(zone - menuIndex);  // Jump to zone
+      selectMenuItem();
+      updateAppState();
+    }
+  }
+}
